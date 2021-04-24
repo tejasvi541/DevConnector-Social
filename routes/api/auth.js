68879ec3d9dca2cdbb824ca9947c8de0114dfeec
Tejasvi <tejasvi.tanwar541@gmail.com>
2021-04-24 19:52:25 +0530

    Updated route auth file

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,8 +7,14 @@ const router = express.Router();
 // @route       GET api/auth
 // @desc        Test route
 // @access      Protected
-router.get("/", auth, (req, res) => {
-  res.send("Auth Route");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
