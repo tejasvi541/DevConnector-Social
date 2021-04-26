e6654a0d0fc46b23cbfe18bbe55a9c2d27e77175
Tejasvi <tejasvi.tanwar541@gmail.com>
2021-04-26 18:00:25 +0530

    Added get and delete post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -45,4 +45,75 @@ router.post(
   }
 );
 
+//==================================== GET All Posts Route =========================//
+// @route       GET api/posts
+// @desc        Get all Posts
+// @access      Private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//==================================== GET Post by id Route =========================//
+// @route       GET api/posts/:id
+// @desc        Get Post by id
+// @access      Private
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === "ObjectId") {
+      res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.status(500).send("Server Error");
+  }
+});
+
+//==================================== Delete Post by id Route =========================//
+// @route       DELETE api/posts/:id
+// @desc        Delete Post by id
+// @access      Private
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      res.status(404).json({ msg: "Post not found" });
+    }
+
+    //Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not Authorized" });
+    }
+    await post.remove();
+
+    res.json({ msg: "Post Removed" });
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === "ObjectId") {
+      res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
