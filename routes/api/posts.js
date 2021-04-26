3daa31041bb16d43b4c907edc5e5e3dd20490dc9
Tejasvi <tejasvi.tanwar541@gmail.com>
2021-04-26 17:39:56 +0530

    Added Post model and create Post Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,13 +1,48 @@
 const express = require("express");
+const auth = require("../../middleware/auth");
+const config = require("config");
+const { check, validationResult } = require("express-validator/check");
+const jwt = require("jsonwebtoken");
+// Requiring Models
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
 // Initialising router
 const router = express.Router();
 
-//==================================== Test User Route =========================//
-// @route       GET api/posts
-// @desc        Test route
-// @access      PUBLIC
-router.get("/", (req, res) => {
-  res.send("posts Route");
-});
+//==================================== Create A POST Route =========================//
+// @route       POST api/posts
+// @desc        Create a Post route
+// @access      Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is Required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
