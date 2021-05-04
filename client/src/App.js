1e9bb1bd5cbcb3c99d548fe579127cc3c5966e39
Tejasvi <tejasvi.tanwar541@gmail.com>
2021-05-04 20:51:58 +0530

    Edit Profile Component

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 import { loadUser } from "./actions/auth";
@@ -49,6 +50,11 @@ const App = () => {
                 exact
                 path="/create-profile"
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
               />
             </Switch>
           </section>
