2bab775d34037dd4334c137fd2907b2a94783c73
Tejasvi <tejasvi.tanwar541@gmail.com>
2021-05-10 22:18:40 +0530

    Post Reducer and Initial Component

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
 
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
@@ -72,6 +73,7 @@ const App = () => {
                 path="/add-education"
                 component={AddEducation}
               />
+              <PrivateRoute exact path="/posts" component={Posts} />
             </Switch>
           </section>
         </Fragment>
