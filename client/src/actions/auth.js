5ebd572d2076c77036a3a3e6f890ce5d1c7cb58b
Tejasvi <tejasvi.tanwar541@gmail.com>
2021-06-23 18:24:18 +0530

    Test Push

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -32,37 +32,39 @@ export const loadUser = () => async (dispatch) => {
 };
 
 // Register User
-export const register = ({ name, email, password }) => async (dispatch) => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+export const register =
+  ({ name, email, password }) =>
+  async (dispatch) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-  const body = JSON.stringify({ name, email, password });
+    const body = JSON.stringify({ name, email, password });
 
-  try {
-    const res = await axios.post("/api/users", body, config);
+    try {
+      const res = await axios.post("/api/users", body, config);
 
-    dispatch({
-      type: REGISTER_SUCCESS,
-      payload: res.data,
-    });
-    dispatch(loadUser());
-  } catch (err) {
-    const errors = err.response.data.errors;
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      });
+      dispatch(loadUser());
+    } catch (err) {
+      const errors = err.response.data.errors;
 
-    if (errors) {
-      errors.forEach((error) => {
-        dispatch(setAlert(error.msg, "danger"));
+      if (errors) {
+        errors.forEach((error) => {
+          dispatch(setAlert(error.msg, "danger"));
+        });
+      }
+
+      dispatch({
+        type: REGISTER_FAIL,
       });
     }
-
-    dispatch({
-      type: REGISTER_FAIL,
-    });
-  }
-};
+  };
 
 // Login User
 export const login = (email, password) => async (dispatch) => {
